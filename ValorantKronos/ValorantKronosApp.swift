8afea3b76907434b4fb1dd74053d5e1896b876e3
Ethan Mont <ethancmm@hotmail.com>
2025-01-03 12:06:37 -0500

    first commit

---
+++ ValorantKronos/ValorantKronosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantKronosApp.swift
+//  ValorantKronos
+//
+//  Created by Ethan Montalvo on 3/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ValorantKronosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ValorantKronosTests/ValorantKronosTests.swift
@@ -0,0 +1,16 @@
+//
+//  ValorantKronosTests.swift
+//  ValorantKronosTests
+//
+//  Created by Ethan Montalvo on 3/01/25.
+//
+
+import Testing
+
+struct ValorantKronosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
